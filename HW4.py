6d85101b9b72a712b7c68d0186f22eff815a1113
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-04-18 22:32:36 +0300

    Add files via upload

---
+++ HW4.py
@@ -0,0 +1,14 @@
+my_string = input('Назовите своё имя: ')
+print('Добро пожаловать, ', my_string.upper())
+
+my_string = input('Назовите слово на букву А в верхнем регистре: ')
+print(my_string.lower())
+
+my_string = input('Напишите предложение из 4 слов: ')
+print(my_string.replace(' ', ''))
+
+my_string = input('Напишите предложение из 4 слов: ')
+print(my_string[0])
+
+my_string = input('Напишите предложение из 4 слов: ')
+print(my_string[-1])
